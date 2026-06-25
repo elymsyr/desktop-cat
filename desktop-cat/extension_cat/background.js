5437e1e180a3d6eb16e2e45e73eedb2d15e17867
Orhun Eren Yalçınkaya <orhunerenyalcinkaya@gmail.com>
2026-06-26 00:28:56 +0300

    update new system

--- desktop-cat/extension_cat/background.js
+++
@@ -1,7 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.query({}, (tabs) => {
-    const tabURLs = tabs.map(tab => tab.url);
-    console.log(tabURLs);
-    // You can perform any action with the list of tab URLs here
-  });
-});
